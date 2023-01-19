7195ce22a648888590eeedf172675396b1faa9cd
wulengbing@163.com <119727958+npumaster@users.noreply.github.com>
2023-01-19 15:42:20 +0800

    add

---
+++ auth/handler.go
@@ -0,0 +1,51 @@
+package auth
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	logging "github.com/ipfs/go-log/v2"
+)
+
+var log = logging.Logger("auth")
+
+type Handler struct {
+	Verify func(ctx context.Context, token string) ([]Permission, error)
+	Next   http.HandlerFunc
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	token := r.Header.Get("Authorization")
+	if token == "" {
+		token = r.FormValue("token")
+		if token != "" {
+			token = "Bearer " + token
+		}
+	}
+
+	if token != "" {
+		if !strings.HasPrefix(token, "Bearer ") {
+			log.Warn("missing Bearer prefix in auth header")
+			w.WriteHeader(401)
+			return
+		}
+		token = strings.TrimPrefix(token, "Bearer ")
+
+		allow, err := h.Verify(ctx, token)
+		if err != nil {
+			log.Warnf("JWT Verification failed (originating from %s): %s", r.RemoteAddr, err)
+			w.WriteHeader(401)
+			return
+		}
+
+		values := make(map[string]any)
+		values["token"] = token
+		values["perms"] = allow
+		ctx = WithPerm(ctx, "params", values)
+	}
+
+	h.Next(w, r.WithContext(ctx))
+}
